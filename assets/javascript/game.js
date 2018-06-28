8aba1f214fd4636d7595ea09cf6ba3aeda3aae53
Carol Martin <carolmartin@cox.net>
2018-06-28 04:48:13 -0700

    added files

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// define a table of Attributes of God 
+
+var attributes = ["patient", "lovingkindness", "sovereignty", "faithfulness", "wisdom", "knowledge"];
+
+
+const maxTries = 15;            // Maximum number of tries player has
+
+var guessedLetters = [];        // Stores the letters the user guessed
+var currentWordIndex;           // Index of the current word in the array
+var guessingWord = [];          // This will be the word we actually build to match the current word
+var remainingGuesses = 0;       // How many tries the player has left
+var gameStarted = false;        // Flag to tell if the game has started
+var hasFinished = false;        // Flag for 'press any key to try again'     
+var wins = 0;                   // How many wins has the player racked up
+
+
+const maxTries = 15;            // Maximum number of tries player has
+
+var guessedLetters = [];        // Stores the letters the user guessed
+var currentWordIndex;           // Index of the current word in the array
+var guessingWord = [];          // This will be the word we actually build to match the current word
+var remainingGuesses = 0;       // How many tries the player has left
+var gameStarted = false;        // Flag to tell if the game has started
+var hasFinished = false;        // Flag for 'press any key to try again'     
+var wins = 0;                   // How many wins has the player racked up
+
+
+
+
+
+// Function to load each individual character in stringToSplit to arrayOfStrings
+
+function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+
+    console.log('The original string is: "' + stringToSplit + '"');
+    console.log('The separator is: "' + separator + '"');
+    console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
+}
